200043bc43b17e598256cb796a3f93a33169df55
Tobias Borlinghaus <tborlinghaus@gmail.com>
2024-01-06 20:38:30 +0100

    Readded Gateway and Changed Logging

--- gateway-service/src/main/java/de/tbo/services/gateway/GatewayApplication.java
+++ gateway-service/src/main/java/de/tbo/services/gateway/GatewayApplication.java
@@ -1,4 +1,4 @@
-package pl.piomin.services.gateway;
+package de.tbo.services.gateway;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
